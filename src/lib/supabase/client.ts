bde2c584a379843e43fd4e7fd030b07e4762a30d
Nibsi3 <cameronfalck03@gmail.com>
2026-01-21 15:04:42 +0200

    c

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -8,6 +8,7 @@ export function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       auth: {
+        flowType: 'pkce',
         persistSession: true,
         autoRefreshToken: true,
         detectSessionInUrl: true,
